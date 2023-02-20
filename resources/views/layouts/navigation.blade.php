3dd6bfe77dc93ee1cf92b586ec5a8928affe4aaa
Mitesh <jadav.sushil@securemetasys.com>
2023-02-20 18:42:38 +0530

    add links to theme element.

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -2,7 +2,21 @@
     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
         {{ __('Dashboard') }}
     </x-nav-link>
-    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-        {{ __('Dashboard') }}
+    <x-nav-link :href="route('siteConfig')" :active="request()->routeIs('siteConfig')">
+        {{ __('Site Config') }}
     </x-nav-link>
+    <li class="menu-item active open">
+        <a href="javascript:void(0);" class="menu-link menu-toggle">
+            <i class="menu-icon tf-icons ti ti-file"></i>
+            <div data-i18n="Pages">Myself</div>
+        </a>
+        <ul class="menu-sub">
+            <x-nav-link :href="route('profile')" :active="request()->routeIs('profile')">
+                {{ __('Profile') }}
+            </x-nav-link>
+            <x-nav-link :href="route('account')" :active="request()->routeIs('account')">
+                {{ __('Account') }}
+            </x-nav-link>
+        </ul>
+    </li>
 </ul>
--- resources/views/layouts/user.blade.php
+++ resources/views/layouts/user.blade.php
@@ -1,5 +1,5 @@
 <li class="nav-item navbar-dropdown dropdown-user dropdown">
-    <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0);"
+    <a class="nav-link dropdown-toggle hide-arrow" href="javascript:void(0)"
        data-bs-toggle="dropdown">
         <div class="avatar avatar-online">
             <img src="/assets/img/profile_placeholder.jpg" alt class="h-auto rounded-circle"/>
@@ -7,7 +7,7 @@
     </a>
     <ul class="dropdown-menu dropdown-menu-end">
         <li>
-            <a class="dropdown-item" href="#">
+            <a class="dropdown-item" href="{{ route('account') }}">
                 <div class="d-flex">
                     <div class="flex-shrink-0 me-3">
                         <div class="avatar avatar-online">
@@ -16,8 +16,8 @@
                         </div>
                     </div>
                     <div class="flex-grow-1">
-                        <span class="fw-semibold d-block">John Doe</span>
-                        <small class="text-muted">Admin</small>
+                        <span class="fw-semibold d-block">{{ auth()->user()->name }}</span>
+                        <small class="text-muted">{{ auth()->user()->roles[0] ?? 'Role not assigned' }}</small>
                     </div>
                 </div>
             </a>
@@ -26,26 +26,26 @@
             <div class="dropdown-divider"></div>
         </li>
         <li>
-            <a class="dropdown-item" href="#">
+            <a class="dropdown-item" href="{{ route('profile') }}">
                 <i class="ti ti-user-check me-2 ti-sm"></i>
                 <span class="align-middle">My Profile</span>
             </a>
         </li>
         <li>
-            <a class="dropdown-item" href="#">
+            <a class="dropdown-item" href="javascript:void(0)">
                 <i class="ti ti-settings me-2 ti-sm"></i>
-                <span class="align-middle">Settings</span>
+                <span class="align-middle"><del>Settings</del></span>
             </a>
         </li>
         <li>
-            <a class="dropdown-item" href="#">
-                        <span class="d-flex align-items-center align-middle">
-                          <i class="flex-shrink-0 ti ti-credit-card me-2 ti-sm"></i>
-                          <span class="flex-grow-1 align-middle">Billing</span>
-                          <span class="flex-shrink-0 badge badge-center rounded-pill bg-label-danger w-px-20 h-px-20"
-                          >2</span
-                          >
-                        </span>
+            <a class="dropdown-item" href="javascript:void(0)">
+                <span class="d-flex align-items-center align-middle">
+                  <i class="flex-shrink-0 ti ti-credit-card me-2 ti-sm"></i>
+                    <span class="flex-grow-1 align-middle"><del>Billing</del></span>
+                  <span class="flex-shrink-0 badge badge-center rounded-pill bg-label-danger w-px-20 h-px-20"
+                  >2</span
+                  >
+                </span>
             </a>
         </li>
         <li>
