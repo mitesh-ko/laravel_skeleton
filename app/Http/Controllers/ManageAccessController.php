2b8f2b8d76facf404998585d73b19cad4c4d5d2f
Mitesh <jadav.sushil@securemetasys.com>
2023-02-21 18:35:58 +0530

    access management

---
+++ app/Http/Controllers/ManageAccessController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ManageAccessController extends Controller
+{
+    public function role(Request $request)
+    {
+        return view('');
+    }
+}
