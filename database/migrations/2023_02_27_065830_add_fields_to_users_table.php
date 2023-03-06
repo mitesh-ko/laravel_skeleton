37ee674bf88996cd444dddd6d0fa5ea47cfcd956
Mitesh <mitesh@gmail.com>
2023-03-06 17:21:45 +0530

    nullable added to new user fields.

--- database/migrations/2023_02_27_065830_add_fields_to_users_table.php
+++ database/migrations/2023_02_27_065830_add_fields_to_users_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      *
@@ -14,8 +13,8 @@ return new class extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->ipAddress('ip');
-            $table->timestamp('login_at');
+            $table->ipAddress('ip')->nullable();
+            $table->timestamp('login_at')->nullable();
         });
     }
 
