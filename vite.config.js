d223bb354f5dd733109098e5678edf24e409fdb6
Mitesh <mitesh@gmail.com>
2023-03-06 17:17:52 +0530

    toaster.js added

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
                 'resources/css/assets/demo.css',
                 'resources/css/assets/profile.css',
                 'resources/css/assets/login.css',
+                //--- js ---
                 'resources/js/app.js',
                 'resources/js/assets/account.js',
                 'resources/js/assets/profile.js',
@@ -16,7 +17,8 @@ export default defineConfig({
                 'resources/js/assets/index-user.js',
                 'resources/js/assets/index-role.js',
                 'resources/js/assets/create-update-role.js',
-                'resources/js/assets/login.js'
+                'resources/js/assets/login.js',
+                'resources/js/assets/toaster.js'
             ],
         }),
     ],
