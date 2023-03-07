7b23d9e3f8eb9d5036f6a2be9e38c845da8cef2d
Mitesh <mitesh@gmail.com>
2023-03-07 17:12:31 +0530

    new method added to send custom mail for password forgot.

--- app/Models/User.php
+++ app/Models/User.php
@@ -16,12 +16,6 @@ class User extends Authenticatable implements Auditable, MustVerifyEmail
 {
     use HasApiTokens, HasFactory, Notifiable, HasRoles, \OwenIt\Auditing\Auditable, SoftDeletes;
 
-
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var array<int, string>
-     */
     protected $fillable = [
         'name',
         'email',
@@ -43,14 +37,20 @@ class User extends Authenticatable implements Auditable, MustVerifyEmail
         'remember_token',
     ];
 
-    /**
-     * The attributes that should be cast.
-     *
-     * @var array<string, string>
-     */
     protected $casts = [
         'email_verified_at' => 'datetime',
-        'created_at' => 'datetime:d-m-Y',
-        'updated_at' => 'datetime:d-m-Y',
+        'created_at'        => 'datetime:d-m-Y',
+        'updated_at'        => 'datetime:d-m-Y',
     ];
+
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new \App\Notifications\ResetPasswordNotification($token, config('resetPasswordMailTemplate')));
+    }
+
+public
+function getFullNameAttribute()
+{
+    return $this->name;
+}
 }
