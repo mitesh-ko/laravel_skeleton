0b69b464b795b3395ab44420ad7ecd7958fea6a0
Mitesh <mitesh@gmail.com>
2023-03-09 17:00:45 +0530

    unnecessary audit log removed.

--- app/Listeners/LoginAuditListener.php
+++ app/Listeners/LoginAuditListener.php
@@ -27,21 +27,5 @@ class LoginAuditListener
      */
     public function handle($event)
     {
-        $user = \Auth::user();
-        $newData = [
-            'ip'       => \Request::getClientIp(),
-            'login_at' => Carbon::now()
-        ];
-        $oldData = [
-            'ip'       => $user->ip,
-            'login_at' => $user->login_at
-        ];
-        User::where('id', \Auth::id())->update($newData);
-
-        $user->auditEvent = 'Login';
-        $user->isCustomEvent = true;
-        $user->auditCustomOld = $oldData;
-        $user->auditCustomNew = $newData;
-        Event::dispatch(AuditCustom::class, [$user]);
     }
 }
---
+++ resources/js/assets/create-update-user.js
@@ -0,0 +1,34 @@
+'use strict';
+
+const formAccSettings = document.querySelector('#formAccountSettings');
+if (formAccSettings) {
+    const fv = FormValidation.formValidation(formAccSettings, {
+        fields: {
+            name: {
+                validators: {
+                    required: {
+                        message: 'Please enter first name'
+                    }
+                }
+            }
+        },
+        plugins: {
+            trigger: new FormValidation.plugins.Trigger(),
+            bootstrap5: new FormValidation.plugins.Bootstrap5({
+                eleValidClass: '',
+                rowSelector: '.col-md-6'
+            }),
+            submitButton: new FormValidation.plugins.SubmitButton(),
+            // Submit the form when all fields are valid
+            defaultSubmit: new FormValidation.plugins.DefaultSubmit(),
+            autoFocus: new FormValidation.plugins.AutoFocus()
+        },
+        init: instance => {
+            instance.on('plugins.message.placed', function (e) {
+                if (e.element.parentElement.classList.contains('input-group')) {
+                    e.element.parentElement.insertAdjacentElement('afterend', e.messageElement);
+                }
+            });
+        }
+    });
+}
